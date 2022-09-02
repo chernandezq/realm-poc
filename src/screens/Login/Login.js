a45499433bdaef067d88834c95decb916d8e74f4
Cristian Hernandez <chernandezq@lean-tech.io>
2022-09-02 08:57:58 -0500

    Login

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,10 +1,15 @@
 import {Realm, useApp} from '@realm/react';
-import React from 'react';
+import {useMutation} from '@tanstack/react-query';
+import React, {useState} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
-import {Button} from 'react-native-paper';
+import {Button, TextInput} from 'react-native-paper';
+import {AuthService} from '../../services';
 
 export const Login = () => {
   const app = useApp();
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
   async function login() {
     let user;
@@ -21,6 +26,23 @@ export const Login = () => {
     }
   }
 
+  const mutation = useMutation(AuthService.logIn, {
+    onSuccess: data => {
+      // Invalidate and refetch
+      console.warn(data);
+    },
+    onError: error => {
+      console.warn(error.response);
+    },
+  });
+
+  const loginMutation = () => {
+    mutation.mutate({
+      username: email,
+      password: password,
+    });
+  };
+
   return (
     <SafeAreaView>
       <View>
@@ -30,7 +52,27 @@ export const Login = () => {
           mode="contained"
           onPress={() => login()}
           style={{marginTop: 20}}>
-          Login
+          Login with Realm
+        </Button>
+
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+        />
+
+        <TextInput
+          label="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+        />
+
+        <Button
+          mode="contained"
+          onPress={() => loginMutation()}
+          disabled={mutation.isLoading}
+          style={{marginTop: 20}}>
+          Login with credentials
         </Button>
       </View>
     </SafeAreaView>
